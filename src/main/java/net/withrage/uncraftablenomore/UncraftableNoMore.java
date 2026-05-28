3065996ef2f425dcec4565032bd4f9ddd415ca43
justerik <sebo.erik@gmail.com>
2026-05-28 16:36:47 +0200

    Initial commit for Fabric 1.21.1 port

---
+++ src/main/java/net/withrage/uncraftablenomore/UncraftableNoMore.java
@@ -0,0 +1,16 @@
+package net.withrage.uncraftablenomore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UncraftableNoMore implements ModInitializer {
+	public static final String MOD_ID = "uncraftablenomore";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Mod Uncraftable No More is loaded!");
+	}
+}
